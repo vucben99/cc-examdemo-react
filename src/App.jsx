9ee2d941f7ef78e564037dfe65334a1e7a5a41c1
Beni Vucsics <vucben99@gmail.com>
2022-12-10 14:47:14 +0100

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,55 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+
+function UserCard({ user }) {
+
+  const [isShown, setIsShown] = useState(false)
 
   return (
-    <div></div>
+    <article className='usercard'>
+      <img src={user.avatar_url} alt={user.login} />
+      <p>{user.login}</p>
+      <button onClick={() => setIsShown(!isShown)}>
+        {isShown ? "Show less" : "Show more"}
+      </button>
+      {isShown && (
+        <div>
+          <p>Rank: {user.type}</p>
+          <p>Admin: {`${user.site_admin}`}</p>
+        </div>)}
+    </article>
+  )
+}
+
+function App() {
+
+  const [users, setUsers] = useState(null)
+  const [search, setSearch] = useState("")
+
+  const getUsers = async () => {
+    const res = await fetch("https://api.github.com/users")
+    const users = await res.json()
+    setTimeout(() => {
+      setUsers(users)
+    }, 1000);
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+
+  return (
+    <>
+      <header className='searchbar'>
+        <input type="text" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)} />
+      </header>
+
+      <main className="content">
+        {users ? users.filter(user => user.login.startsWith(search)).map(user => <UserCard user={user} key={user.login} />) : <p>Loading...</p>}
+      </main>
+    </>
   )
 }
 
